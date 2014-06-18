9b21b590e1ff548884de81d21b75b74409fb18e3
Sandi Metz <skmetz@gmail.com>
2014-06-18 10:53:06 -0400

    This should work for everything, compile it as the else branch.

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -33,6 +33,8 @@ class House
       "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     when 12
       "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+    else
+      "This is #{phrase(num)}.\n"
     end
   end
 
