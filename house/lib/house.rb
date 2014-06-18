9486058a00f399c20f00032145438963f749dcfb
Sandi Metz <skmetz@gmail.com>
2014-06-18 10:55:10 -0400

    Delete 3 from case statement; fall through to the else.

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -9,8 +9,6 @@ class House
 
   def line(num)
     case num
-    when 3
-      "This is the rat that ate the malt that lay in the house that Jack built.\n"
     when 4
       "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     when 5
