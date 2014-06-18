bd77ab846302d4431f88cafa56c43fd08c7105cc
Sandi Metz <skmetz@gmail.com>
2014-06-18 10:52:17 -0400

    Delete obsolete code.

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -12,7 +12,6 @@ class House
     when 1
       "This is #{phrase(num)}.\n"
     when 2
-      "This is the malt that lay in #{phrase}.\n"
       "This is #{phrase(num)}.\n"
     when 3
       "This is the rat that ate the malt that lay in the house that Jack built.\n"
