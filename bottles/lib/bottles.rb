2a3cacd619a25e762649b706135b746f05a9c2da
Sandi Metz <skmetz@gmail.com>
2014-07-16 10:11:34 -0400

    Pass the number-1 argument when sending #amount.

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -13,7 +13,7 @@ class Bottles
     when 0
       "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
     when 1
-      "#{number} bottle of beer on the wall, #{number} bottle of beer.\nTake #{pronoun(number)} down and pass it around, #{amount} bottles of beer on the wall.\n"
+      "#{number} bottle of beer on the wall, #{number} bottle of beer.\nTake #{pronoun(number)} down and pass it around, #{amount(number-1)} bottles of beer on the wall.\n"
     else
       "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake #{pronoun(number)} down and pass it around, #{number-1} #{container(number-1)} of beer on the wall.\n"
     end
