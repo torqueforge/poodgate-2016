306aa0b2c95c93cbcdbc459468a899e48addc8ba
Sandi Metz <skmetz@gmail.com>
2014-07-16 10:34:23 -0400

    Add 'Go to the store' to #action.

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -42,6 +42,10 @@ class Bottles
   end
 
   def action(number)
-    "Take #{pronoun(number)} down and pass it around"
+    if number == 0
+      "Go to the store and buy some more"
+    else
+      "Take #{pronoun(number)} down and pass it around"
+    end
   end
 end
