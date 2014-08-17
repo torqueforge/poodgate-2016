a0405b0633159fd54c9d0c9a0ec189d37b7b343e
Sandi Metz <skmetz@gmail.com>
2014-08-17 08:29:14 -0400

    Remove obsolete private methods Bottles.
    We're starting the cleanup.  First I'll remove unneeded methods from Bottles.

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -16,47 +16,6 @@ class Bottles
     "#{bottle_number.action}, " +
     "#{next_bottle_number.amount} #{next_bottle_number.container} of beer on the wall.\n"
   end
-
-  private
-  def container(bottle_number)
-    if bottle_number == 1
-      "bottle"
-    else
-      "bottles"
-    end
-  end
-
-  def pronoun(bottle_number)
-    if bottle_number == 1
-      "it"
-    else
-      "one"
-    end
-  end
-
-  def amount(bottle_number)
-    if bottle_number == 0
-      "no more"
-    else
-      bottle_number.to_s
-    end
-  end
-
-  def action(bottle_number)
-    if bottle_number == 0
-      "Go to the store and buy some more"
-    else
-      "Take #{pronoun(bottle_number)} down and pass it around"
-    end
-  end
-
-  def successor(bottle_number)
-    if bottle_number == 0
-      99
-    else
-      bottle_number - 1
-    end
-  end
 end
 
 class BottleNumber
