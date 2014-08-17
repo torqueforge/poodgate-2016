10cb41922673ced9317dccd62b3671cae2d3022a
Sandi Metz <skmetz@gmail.com>
2014-08-17 11:04:41 -0400

    Change #verse to get the 'next bottle number' by asking for the current's successor.

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -9,7 +9,7 @@ class Bottles
 
   def verse(number)
     bottle_number      = bottle_number_for(number)
-    next_bottle_number = bottle_number_for(bottle_number.successor)
+    next_bottle_number = bottle_number.successor
     "#{bottle_number.amount.capitalize} #{bottle_number.container} of beer on the wall, " +
     "#{bottle_number.amount} #{bottle_number.container} of beer.\n" +
     "#{bottle_number.action}, " +
