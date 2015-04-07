43c6db53c7a32ad6ef5efaebf4554391ee9bd488
Sandi Metz <sandi@sandimetz.com>
2015-04-07 14:13:40 -0400

    Pass the num argument in existing code.

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -17,7 +17,7 @@ class House
     when 1
       "This is the house that Jack built.\n"
     when 2
-      "This is #{phrase}the house that Jack built.\n"
+      "This is #{phrase(num)}the house that Jack built.\n"
     when 3
       "This is the rat that ate the malt that lay in the house that Jack built.\n"
     when 4
