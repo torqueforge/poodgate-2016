e8f8db1632a8291a3220c98ccea3ac0992741cdd
Sandi Metz <sandi@sandimetz.com>
2015-04-07 17:17:51 -0400

    Refactor the DRY house to ease RandomHouse.
    This refactoring changes a few methods to have one responsibility, and it
    replaces '' with 'the house that Jack built'.
    
    One could argue that 'the house that Jack built' _is_ correct, and this makes
    RandomHouse easier.  It feels more important to teach them to proceed
    from here, than to teach them how to get here from house_2_shameles_to_dry.

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -1,25 +1,31 @@
 class House
+  DATA = [
+    'the horse and the hound and the horn that belonged to',
+    'the farmer sowing his corn that kept',
+    'the rooster that crowed in the morn that woke',
+    'the priest all shaven and shorn that married',
+    'the man all tattered and torn that kissed',
+    'the maiden all forlorn that milked',
+    'the cow with the crumpled horn that tossed',
+    'the dog that worried',
+    'the cat that killed',
+    'the rat that ate',
+    'the malt that lay in',
+    'the house that Jack built']
 
   def recite
     1.upto(12).collect {|i| line(i)}.join("\n")
   end
 
   def line(num)
-    "This is #{phrase(num)}the house that Jack built.\n"
+    "This is #{phrase(num)}.\n"
   end
 
   def phrase(num)
-    [ 'the horse and the hound and the horn that belonged to',
-      'the farmer sowing his corn that kept',
-      'the rooster that crowed in the morn that woke',
-      'the priest all shaven and shorn that married',
-      'the man all tattered and torn that kissed',
-      'the maiden all forlorn that milked',
-      'the cow with the crumpled horn that tossed',
-      'the dog that worried',
-      'the cat that killed',
-      'the rat that ate',
-      'the malt that lay in',
-      ''].last(num).join(' ')
+    data.last(num).join(' ')
+  end
+
+  def data
+    DATA
   end
 end
