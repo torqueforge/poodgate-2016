0df2c208c6c92d207fac51273c5c27ce68f6e60e
Sandi Metz <sandi@sandimetz.com>
2015-04-07 17:36:24 -0400

    Random via primitive obsession on a boolean.
    This breaks all of the refactoring rules.  It's just a spike to explore the problem,
    and it has the advantage of expressing the problem in a form we've seen before.
    The 'if' statement in #initialize looks a lot like those methods we wrote for 99
    Bottles.
    
    If only that boolean were smarter......

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -13,6 +13,16 @@ class House
     'the malt that lay in',
     'the house that Jack built']
 
+  attr_reader :data
+  def initialize(random=false)
+    @data =
+      if random
+        DATA.shuffle
+      else
+        DATA
+      end
+  end
+
   def recite
     1.upto(12).collect {|i| line(i)}.join("\n")
   end
@@ -23,9 +33,5 @@ class House
 
   def phrase(num)
     data.last(num).join(' ')
-  end
-
-  def data
-    DATA
   end
 end
