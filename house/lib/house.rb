8c7bb597849115a2d74439c418eb9b23efdc3016
Sandi Metz <sandi@sandimetz.com>
2015-04-07 14:32:31 -0400

    Remove trailing ' ' from bits, join on ' ' instead.

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -5,7 +5,7 @@ class House
   end
 
   def phrase(num)
-    ['the rat that ate ', 'the malt that lay in ', ''].last(num).join('')
+    ['the rat that ate', 'the malt that lay in', ''].last(num).join(' ')
   end
 
   def line(num)
