edd685b27334b96e5264ef9338b07ee99abd99f4
Sandi Metz <sandi@sandimetz.com>
2015-04-07 14:05:07 -0400

    First use of #phrase method.

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -13,7 +13,7 @@ class House
     when 1
       "This is the house that Jack built.\n"
     when 2
-      "This is the malt that lay in the house that Jack built.\n"
+      "This is #{phrase}the house that Jack built.\n"
     when 3
       "This is the rat that ate the malt that lay in the house that Jack built.\n"
     when 4
