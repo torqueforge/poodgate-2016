ddcdafbc3fd9299f09dc54cec6a5bdaba3bae688
Sandi Metz <sandi@sandimetz.com>
2015-04-14 13:22:32 -0400

    upgrade minitest, remove 'instructions'

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,6 +1,3 @@
-# Feel free to delete the instructions once you get going
-puts instructions(__FILE__)
-
 class Bottles
 
 end
--- bottles/test/bottles_test.rb
+++ bottles/test/bottles_test.rb
@@ -1,6 +1,3 @@
-gem 'minitest', '~> 5.5'
-require 'minitest/autorun'
-require 'minitest/pride'
 require_relative '../../test_helper'
 require_relative '../lib/bottles'
 
