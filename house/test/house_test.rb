2e7b1ebc368927d02eef7ee454451c4726ef16ed
Sandi Metz <sandi@sandimetz.com>
2015-07-21 13:32:38 -0400

    shameless house

--- house/test/house_test.rb
+++ house/test/house_test.rb
@@ -13,73 +13,61 @@ class HouseTest < Minitest::Test
   end
 
   def test_line_2
-    skip
     expected = "This is the malt that lay in the house that Jack built.\n"
     assert_equal expected, tale.line(2)
   end
 
   def test_line_3
-    skip
     expected = "This is the rat that ate the malt that lay in the house that Jack built.\n"
     assert_equal expected, tale.line(3)
   end
 
   def test_line_4
-    skip
     expected = "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     assert_equal expected, tale.line(4)
   end
 
   def test_line_5
-    skip
     expected = "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     assert_equal expected, tale.line(5)
   end
 
   def test_line_6
-    skip
     expected = "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     assert_equal expected, tale.line(6)
   end
 
   def test_line_7
-    skip
     expected = "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     assert_equal expected, tale.line(7)
   end
 
   def test_line_8
-    skip
     expected = "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     assert_equal expected, tale.line(8)
   end
 
   def test_line_9
-    skip
     expected = "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     assert_equal expected, tale.line(9)
   end
 
   def test_line_10
-    skip
     expected = "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     assert_equal expected, tale.line(10)
   end
 
   def test_line_11
-    skip
     expected = "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     assert_equal expected, tale.line(11)
   end
 
   def test_line_12
-    skip
     expected = "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     assert_equal expected, tale.line(12)
   end
 
   def test_all_the_lines
-    skip
     expected = <<-TEXT
 This is the house that Jack built.
 
