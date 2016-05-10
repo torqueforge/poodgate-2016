9d9f75782c325b45cc2ec15da874cd74d0e36480
Sandi Metz <sandi@sandimetz.com>
2016-05-10 15:23:52 -0700

    bottles extract class

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -9,13 +9,24 @@ class Bottles
   end
 
   def verse(number)
-    "#{label(number).capitalize} #{container(number)} of beer on the wall, " +
-    "#{label(number)} #{container(number)} of beer.\n" +
-    "#{action(number)}, " +
-    "#{label(successor(number))} #{container(successor(number))} of beer on the wall.\n"
+    container_number = ContainerNumber.new(number)
+    successor        = ContainerNumber.new(container_number.successor)
+
+    "#{container_number.label.capitalize} #{container_number.container} of beer on the wall, " +
+    "#{container_number.label} #{container_number.container} of beer.\n" +
+    "#{container_number.action}, " +
+    "#{successor.label} #{successor.container} of beer on the wall.\n"
+  end
+end
+
+class ContainerNumber
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
   end
 
-  def container(number)
+  def container
     if number == 1
       "bottle"
     else
@@ -23,7 +34,7 @@ class Bottles
     end
   end
 
-  def pronoun(number)
+  def pronoun
     if number == 1
       "it"
     else
@@ -31,7 +42,7 @@ class Bottles
     end
   end
 
-  def label(number)
+  def label
     if number == 0
       "no more"
     else
@@ -39,15 +50,15 @@ class Bottles
     end
   end
 
-  def action(number)
+  def action
     if number == 0
       "Go to the store and buy some more"
     else
-      "Take #{pronoun(number)} down and pass it around"
+      "Take #{pronoun} down and pass it around"
     end
   end
 
-  def successor(number)
+  def successor
     if number == 0
       99
     else
